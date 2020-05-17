a5eb82580a07aa70e778440366076ad765f39192
Raúl Mortes <correomortes@gmail.com>
2020-05-17 08:04:42 +0200

    Añadido componente

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="left-container"><Clock /></div>
+    <div class="right-container"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "./components/HelloWorld.vue";
+import Clock from "./components/Clock.vue";
+
+require('@/App.css')
 
 @Component({
   components: {
-    HelloWorld
+    HelloWorld,
+    Clock
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style>
+body {
+  background-color: #2c3e50;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #EEE;
   margin-top: 60px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr ;
+  gap: 1px 1px;
+  grid-template-areas: ". .";
 }
+
+#left-container {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  gap: 1px 1px;
+  grid-template-areas: "." "." ".";
+}
+
+#right-container {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  gap: 1px 1px;
+  grid-template-areas: "." "." "." ".";
+}
+
+
 </style>
